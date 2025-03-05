c9f9e9757efb58dd0f1bd60d67dcf13851295855
rammeneses <rmmeneses@up.edu.ph>
2025-03-05 22:01:50 +0800

    feat: added more POST statements to serve as tests.

--- test.js
+++ test.js
@@ -22,7 +22,74 @@ needle.post(
         yearPublished:1997   
     },
     (err, res) => {
-        console.log(res.body) // prints the server’s response “Received a POSTrequest.”
+        console.log(res.body)
+    }
+)
+
+// More Post Requests for testing
+// Duplicate of the first POST
+needle.post(
+    'http://localhost:3000/add-book',
+    {
+        bookName: "Harry Potter and the Philosopher's Stone",
+        isbn: "978-0-7475-3269-9",
+        author: "J.K Rowling",
+        yearPublished:1997   
+    },
+    (err, res) => {
+        console.log(res.body)
+    }
+)
+// Diff book name
+needle.post(
+    'http://localhost:3000/add-book',
+    {
+        bookName: "Harry Potter and the Transmutation Table",
+        isbn: "978-0-7475-3269-9",
+        author: "J.K Rowling",
+        yearPublished:1997   
+    },
+    (err, res) => {
+        console.log(res.body)
+    }
+)
+// Diff author
+needle.post(
+    'http://localhost:3000/add-book',
+    {
+        bookName: "Harry Potter and the Philosopher's Stone",
+        isbn: "978-0-7475-3269-9",
+        author: "J.K Rowling in the deep",
+        yearPublished:1997   
+    },
+    (err, res) => {
+        console.log(res.body)
+    }
+)
+// Diff year
+needle.post(
+    'http://localhost:3000/add-book',
+    {
+        bookName: "Harry Potter and the Philosopher's Stone",
+        isbn: "978-0-7475-3269-9",
+        author: "J.K Rowling",
+        yearPublished:1998   
+    },
+    (err, res) => {
+        console.log(res.body)
+    }
+)
+// Diff isbn
+needle.post(
+    'http://localhost:3000/add-book',
+    {
+        bookName: "Harry Potter and the Philosopher's Stone",
+        isbn: "123978-1230-1237475-1233269-1239",
+        author: "J.K Rowling",
+        yearPublished:1997   
+    },
+    (err, res) => {
+        console.log(res.body)
     }
 )
 
